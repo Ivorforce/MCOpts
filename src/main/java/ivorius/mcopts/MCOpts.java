333ffa4d27fea32540ffd9434d9134064dc9a4d0
Lukas Tenbrink <lukastenbrink@googlemail.com>
2017-06-09 15:23:27 +0200

    Fix VERSION label, distribute gradle with sources and reformat build.gradle a bit.

--- src/main/java/ivorius/mcopts/MCOpts.java
+++ src/main/java/ivorius/mcopts/MCOpts.java
@@ -16,7 +16,7 @@ public class MCOpts
 {
     public static final String NAME = "MCOpts";
     //    public static final String MOD_ID = "mcopts";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "0.9";
 
     public static Logger logger = LogManager.getLogger(NAME);
 

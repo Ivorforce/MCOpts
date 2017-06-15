a34ce2d0689069fd187cdc5579ea29da6b31c22c
Lukas Tenbrink <lukastenbrink@googlemail.com>
2017-06-15 13:43:45 +0200

    Bump version to 0.9.3.4.

--- src/main/java/ivorius/mcopts/MCOpts.java
+++ src/main/java/ivorius/mcopts/MCOpts.java
@@ -16,7 +16,7 @@ public class MCOpts
 {
     public static final String NAME = "MCOpts";
     //    public static final String MOD_ID = "mcopts";
-    public static final String VERSION = "0.9.3.3";
+    public static final String VERSION = "0.9.3.4";
 
     public static Logger logger = LogManager.getLogger(NAME);
 

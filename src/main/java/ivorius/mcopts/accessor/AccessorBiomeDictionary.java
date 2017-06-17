bb06c67e47347fc302c33bbd11309db750b4ff4a
Lukas Tenbrink <lukastenbrink@googlemail.com>
2017-06-17 11:35:57 +0200

    Fix AccessorBiomeDictionary.

--- src/main/java/ivorius/mcopts/accessor/AccessorBiomeDictionary.java
+++ src/main/java/ivorius/mcopts/accessor/AccessorBiomeDictionary.java
@@ -7,19 +7,38 @@ package ivorius.mcopts.accessor;
 
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
+import com.google.gson.annotations.SerializedName;
 import ivorius.mcopts.reflection.SafeReflector;
 import net.minecraftforge.common.BiomeDictionary;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by lukas on 08.06.17.
  */
 public class AccessorBiomeDictionary
 {
+    public static String serializedName(Enum anEnum)
+    {
+        String name = anEnum.name();
+
+        try
+        {
+            SerializedName serializedName = anEnum.getClass().getField(name).getAnnotation(SerializedName.class);
+
+            if (serializedName != null)
+            {
+                name = serializedName.value();
+            }
+        }
+        catch (NoSuchFieldException ignored)
+        {
+
+        }
+
+        return name;
+    }
+
     public static BiomeDictionary.Type getTypeWeak(String var)
     {
         return getMap().get(var.toUpperCase());
@@ -27,7 +46,12 @@ public class AccessorBiomeDictionary
 
     public static Map<String, BiomeDictionary.Type> getMap()
     {
-        return SafeReflector.get(BiomeDictionary.Type.class, "byName", null);
+        HashMap<String, BiomeDictionary.Type> map = new HashMap<>();
+
+        for (BiomeDictionary.Type type : BiomeDictionary.Type.values())
+            map.put(serializedName(type), type);
+
+        return map;
     }
 
     public static void addSubtypes(BiomeDictionary.Type type, BiomeDictionary.Type... subtypes)
@@ -42,11 +66,11 @@ public class AccessorBiomeDictionary
 
     public static List<BiomeDictionary.Type> getSubtypes(BiomeDictionary.Type type)
     {
-        return SafeReflector.get(BiomeDictionary.Type.class, "subTypes", type, new ArrayList<>());
+        return SafeReflector.get(BiomeDictionary.Type.class, "subTags", type, new ArrayList<>());
     }
 
     public static void setSubtypes(BiomeDictionary.Type type, List<BiomeDictionary.Type> types)
     {
-        SafeReflector.of(BiomeDictionary.Type.class, "subTypes", field -> field.set(type, types));
+        SafeReflector.of(BiomeDictionary.Type.class, "subTags", field -> field.set(type, types));
     }
 }
